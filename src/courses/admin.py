38023d10e86629d3f0bd9b58773a57bd36bde27c
Saga Charya <sagacharya77@gmail.com>
2025-03-18 23:51:05 +0545

    implemented cloudinary for images

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Course
+from django.utils.html import format_html
+from cloudinary import CloudinaryImage
 
-# Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'access', 'status']
+    list_filter = ['access', 'status']
+    fields = ['title', 'description', 'image', 'access', 'status', 'display_image']
+    readonly_fields = ['display_image']
+    
+    def display_image(self,obj, *args, **kwargs):
+        url = obj.image.url
+        cloudinary_id = str(obj.image)
+        cloudinary_html =CloudinaryImage(cloudinary_id).image(width=500)
+        return format_html(cloudinary_html)
+
+    display_image.short_description = 'Current Image'
+        
+
+
+# admin.site.register(Course)
--- src/courses/models.py
+++ src/courses/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+import helpers
+from cloudinary.models import CloudinaryField
+
+
+helpers.cloudinary_init()
 
 
 class AccessRequirement(models.TextChoices):
@@ -19,7 +24,8 @@ def handle_upload(instance, filename):
 class Course(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    image = models.ImageField(upload_to=handle_upload, blank=True, null=True)
+    # image = models.ImageField(upload_to=handle_upload, blank=True, null=True)
+    image = CloudinaryField('image',null=True)
     access = models.CharField(
         max_length=5,
         choices=AccessRequirement.choices,
