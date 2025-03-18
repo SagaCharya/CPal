5be01b41c98d3f52e1ba241a549d034f8486aa3c
Saga Charya <sagacharya77@gmail.com>
2025-03-18 22:45:49 +0545

    7 - makemigration and migrate

--- src/courses/models.py
+++ src/courses/models.py
@@ -1,22 +1,31 @@
 from django.db import models
 
 
-
 class AccessRequirement(models.TextChoices):
     ANYONE = "any", "Anyone"
-    EMAIL_REQUIRED = 'email_required', 'Email Required'
+    EMAIL_REQUIRED = "email", "Email Required"
+
 
 class PublishStatus(models.TextChoices):
     PUBLISHED = "pub", "Published"
-    DRAFT = 'draft', 'Draft'
-    COMING_SOON = 'soon', 'Coming Soon'
-    
-    
+    DRAFT = "draft", "Draft"
+    COMING_SOON = "soon", "Coming Soon"
+
+
+def handle_upload(instance, filename):
+    return f"{filename}"
 
 
 class Course(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    # image= models.ImageField()
-    access = models.CharField(max_length=20, choices=AccessRequirement.choices, default=AccessRequirement.ANYONE)
-    status = models.CharField(max_length=10, choices=PublishStatus.choices, default=PublishStatus.DRAFT)
+    image = models.ImageField(upload_to=handle_upload, blank=True, null=True)
+    access = models.CharField(
+        max_length=5,
+        choices=AccessRequirement.choices,
+        default=AccessRequirement.EMAIL_REQUIRED,
+    )
+
+    status = models.CharField(
+        max_length=10, choices=PublishStatus.choices, default=PublishStatus.DRAFT
+    )
