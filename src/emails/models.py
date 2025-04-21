ab1f68ee1331779e9ba5e8d479e49746a73aacf3
Saga Charya <sagacharya77@gmail.com>
2025-04-21 12:41:06 +0545

    email form error fixed

--- src/emails/models.py
+++ src/emails/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Email(models.Model):
-    email = (models.EmailField(unique=True),)
+    email = models.EmailField(unique=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
 
